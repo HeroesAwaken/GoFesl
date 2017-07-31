342d0806df366d1a2909e4f7bbeed773cb6db857
Spencer Sharkey <spencer@sf-n.com>
2017-07-30 21:28:52 -0400

    add external matchmaking api call

--- matchmaking/matchmaking.go
+++ matchmaking/matchmaking.go
@@ -1,18 +1,38 @@
 package matchmaking
 
-import "github.com/SpencerSharkey/GoFesl/GameSpy"
+import (
+	"crypto/tls"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/SpencerSharkey/GoFesl/GameSpy"
+	"github.com/SpencerSharkey/GoFesl/log"
+)
 
 // Games - a list of available games
 var Games = make(map[string]*GameSpy.Client)
 
 // FindAvailableGID - returns a GID suitable for the player to join (ADD A PID HERE)
-func FindAvailableGID() string {
+func FindAvailableGID(heroID string) string {
 
-	var gameID string
-
-	for k := range Games {
-		gameID = k
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
 	}
 
-	return gameID
+	client := &http.Client{Transport: tr}
+	resp, err := client.Get("https://heroesawaken.org/api/matchmaking/findgame/" + heroID)
+	if err != nil {
+		log.Warningln("Error making request to matchmaking api")
+		return "0"
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Warningln("Error reading from response to matchmaking api")
+		return "0"
+	}
+
+	return string(body[:])
 }
