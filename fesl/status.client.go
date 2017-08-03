434f22d2e6669a899cd6f7d49468d65c54c3a812
Spencer <spencer@sf-n.com>
2017-08-03 11:12:57 -0400

    send "denied" for MM failures (AKA list of 0 games)

--- fesl/status.client.go
+++ fesl/status.client.go
@@ -23,6 +23,7 @@ func (fM *FeslManager) Status(event GameSpy.EventClientTLSCommand) {
 	// Check if user is allowed to matchmake
 	if !fM.userHasPermission(event.Client.RedisState.Get("uID"), "game.matchmake") {
 		log.Noteln("User not worthy: " + event.Client.RedisState.Get("username"))
+		sendDenied(event)
 		return
 	}
 
@@ -30,6 +31,8 @@ func (fM *FeslManager) Status(event GameSpy.EventClientTLSCommand) {
 	rows, err := fM.getStatsStatement(2).Query(event.Client.RedisState.Get("heroID"), event.Client.RedisState.Get("uID"), "c_eqp", "c_apr")
 	if err != nil {
 		log.Errorln("Failed gettings stats for hero "+event.Client.RedisState.Get("heroID"), err.Error())
+		sendDenied(event)
+		return
 	}
 
 	stats := make(map[string]string)
@@ -43,17 +46,7 @@ func (fM *FeslManager) Status(event GameSpy.EventClientTLSCommand) {
 	}
 
 	if strings.Contains(stats["c_eqp"], "3018") {
-		log.Noteln("User trying to matchmake with op launcher")
-		answer := make(map[string]string)
-		answer["TXN"] = "Status"
-		answer["id.id"] = "1"
-		answer["id.partition"] = event.Command.Message["partition.partition"]
-		answer["sessionState"] = "COMPLETE"
-		answer["props.{}.[]"] = "2"
-		answer["props.{resultType}"] = "JOIN"
-		answer["props.{games}.[]"] = "0"
-		event.Client.WriteFESL("pnow", answer, 0x80000000)
-		fM.logAnswer("pnow", answer, 0x80000000)		
+		sendDenied(event)
 		return
 	}
 
@@ -71,49 +64,27 @@ func (fM *FeslManager) Status(event GameSpy.EventClientTLSCommand) {
 	ipint := binary.BigEndian.Uint32(event.Client.IpAddr.(*net.TCPAddr).IP.To4())
 	gameIDs := matchmaking.FindAvailableGIDs(event.Client.RedisState.Get("heroID"), fmt.Sprint(ipint))
 
-	i := 0
 	for _, gid := range gameIDs {
-		gameID := gid
-
-		// Get percent_full from databse
-		var args []interface{}
-		statsKeys := make(map[string]string)
-		args = append(args, gameID)
-		args = append(args, "B-U-percent_full")
-
-		rows, err := fM.getServerStatsVariableAmount(1).Query(args...)
-		if err != nil {
-			log.Errorln("Failed gettings stats for hero "+gameID, err.Error())
-		}
-
-		for rows.Next() {
-			var gid, statsKey, statsValue string
-			err := rows.Scan(&gid, &statsKey, &statsValue)
-			if err != nil {
-				log.Errorln("Issue with database:", err.Error())
-			}
-			statsKeys[statsKey] = statsValue
-		}
-
-		if statsKeys["B-U-percent_full"] == "100" {
-			// Don't add for matchmaking if server is full
-			continue
-		}
-
-		gameServer := new(lib.RedisObject)
-		gameServer.New(fM.redis, "gdata", gameID)
-
 		answer["props.{games}."+strconv.Itoa(i)+".lid"] = "1"
-		//answer["props.{games}."+strconv.Itoa(i)+".fit"] = strconv.Itoa(len(matchmaking.Games) - i)
 		answer["props.{games}."+strconv.Itoa(i)+".fit"] = "1000"
-		answer["props.{games}."+strconv.Itoa(i)+".gid"] = gameID
-
-		log.Noteln(gameServer.Get("NAME") + " GID: " + gameID + " with fitness of: " + strconv.Itoa(len(matchmaking.Games)-i))
-		i++
+		answer["props.{games}."+strconv.Itoa(i)+".gid"] = gid
 	}
 
-	answer["props.{games}.[]"] = strconv.Itoa(i)
+	answer["props.{games}.[]"] = len(gameIDs)
 
+	event.Client.WriteFESL("pnow", answer, 0x80000000)
+	fM.logAnswer("pnow", answer, 0x80000000)
+}
+
+func sendDenied(event GameSpy.EventClientTLSCommand) {
+	answer := make(map[string]string)
+	answer["TXN"] = "Status"
+	answer["id.id"] = "1"
+	answer["id.partition"] = event.Command.Message["partition.partition"]
+	answer["sessionState"] = "COMPLETE"
+	answer["props.{}.[]"] = "2"
+	answer["props.{resultType}"] = "JOIN"
+	answer["props.{games}.[]"] = "0"
 	event.Client.WriteFESL("pnow", answer, 0x80000000)
 	fM.logAnswer("pnow", answer, 0x80000000)
 }
